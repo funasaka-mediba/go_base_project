1f2932b7c15a2ee059bc680c29c108711dba0e79
funasaka-mediba <funasaka@mediba.jp>
2022-11-30 01:28:14 +0900

    zapの使い方・設定の仕方を学びながら実装

--- api/main.go
+++ api/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"go_base_project/packages/env"
+	"go_base_project/packages/log"
 	"go_base_project/registry"
 	"net/http"
+	"os"
 )
+
+var version string
 
 func main() {
 	// setupEnv()
-	// setupLogger()
+	setupLogger()
 	addr := fmt.Sprintf("%s:%s", env.Env().ListenHost, env.Env().ListenPort)
 
 	registry := registry.NewRegistry()
@@ -23,4 +27,17 @@ func main() {
 	fmt.Println(server)
 
 	// TODO: graceful restart&stop
+}
+
+func setupLogger() {
+	lg, err := log.NewLogger(&log.Config{
+		Env:        os.Getenv("APPLICATION_ENV"),
+		LogLevel:   os.Getenv("LOG_LEVEL"),
+		AppName:    os.Getenv("APP_NAME"),
+		AppVersion: version,
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.SetLogger(lg)
 }
